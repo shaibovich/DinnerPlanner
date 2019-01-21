73f9577d151dd4ae6874624bd1eb84df13e57d50
shaibovich <shaibovich@gmail.com>
2019-01-21 21:31:12 +0200

    .

--- services/user_recipe_service.py
+++ services/user_recipe_service.py
@@ -1,24 +1,21 @@
 from services.abstract_service import abstrac_service
-from mysql.tabels import user_recipe, meal_dishes
+from mysql.tabels import user_recipe
+
 
 class user_recipe_service(abstrac_service):
     def __init__(self, my_sql):
         abstrac_service.__init__(self, my_sql)
 
-
     def add_or_update_dish(self, request):
         query, params = user_recipe.get_user_recipe(request['user'], request['dish_id'])
         if self.db.is_exists(query, params):
-            query,params = user_recipe.update_recipe(request)
+            query, params = user_recipe.update_recipe(request)
         else:
             query, params = user_recipe.insert(request)
         result = self.db.insert(query, params)
         return self.return_success(result)
 
-
     def delete_dish_recipe(self, user_id, dish_id):
-        query, params = user_recipe.delete_user_recipe(user_id,dish_id)
+        query, params = user_recipe.delete_user_recipe(user_id, dish_id)
         self.db.delete(query, params)
         return self.return_success('success')
-
-
